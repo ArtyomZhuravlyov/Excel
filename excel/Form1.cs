f2707addf3036c635a163251b191337a00aa69c5
Zhuravlyov Artyom <Zhuravlyov.Artyom.spbk@mail.ru>
2018-11-28 17:46:21 +0300

    добавил

--- excel/Form1.cs
+++ excel/Form1.cs
@@ -12,7 +12,15 @@ using Excel = Microsoft.Office.Interop.Excel;
 namespace excel
 {
     public partial class Form1 : Form
-    {    //общее //приложение и книги
+    {
+        public interface Prototipe
+        { 
+            void writtenIPT(int offset); //int offset в прототипе можно не указывать
+            void writtenBU(int offset);           
+        }
+        
+
+        //общее //приложение и книги
         private Excel.Application excelapp;
         private Excel.Workbooks excelappworkbooks;
 
@@ -46,13 +54,39 @@ namespace excel
             public static int Quantityrow = 0;
         }
 
-        public struct SItemsListBox
+        public struct SItemsListBox //если использую static не могу создать объекты IPT;BU;
         {
-            public static string[] Items = new string[6];
-            public static string[] PathFile = new string[6];
-            public static int QuantityFiles = 0;
+            public  string[] Items; //= { "", "", "" };
+            public  string[] PathFile;//= { "", "", "" };
+            public  int QuantityFiles ; //=0;
+
+            public SItemsListBox(string[] Items, string[] PathFile, int QuantityFiles)
+            {
+                this.Items = Items; // хранит имя файла
+                this.PathFile = PathFile; //хранит путь к файлу
+                this.QuantityFiles = QuantityFiles; // кол-во файлов
+            }
+
+            public void clear()
+            {
+                Items = new string[3];
+                Items[0] = "";
+                Items[1] = "";
+                Items[2] = "";
+
+                PathFile = new string[3];
+                PathFile[0] = "";
+                PathFile[1] = "";
+                PathFile[2] = "";
+
+                QuantityFiles = 0;
+            }
         }
 
+        SItemsListBox BU; //т.к. 2 бокса на файлы
+        SItemsListBox IPT;
+
+        //такая запись работает если не создаются объекты этой структуры
         public struct Stime
         {
             public static string[] seconds_05 = {"00", "05", "10" , "15" , "20", "25", "30","35", "40","45", "50", "55" };
@@ -68,23 +102,22 @@ namespace excel
         public Form1()
         {
             InitializeComponent();
+            BU.clear();
+            IPT.clear();
         }
 
-        private void Form1_Load(object sender, EventArgs e)
-        {
-
-        }
+        
 
         private void button1_Click(object sender, EventArgs e)
         {
-            
+             
             int i = Convert.ToInt32(((Button)(sender)).Tag);
-            SItemsListBox.QuantityFiles = 1; //!! временно
-            if (SItemsListBox.QuantityFiles == 0 && i == 1 ) return; //i=1 первая кнопка 
-                                                                     //разобраться почему не сработало String.Empty
-              // SItemsListBox.PathFile[0] = @"C:\Users\Zhuravlev\Desktop\для проги\ИПТ4.xlsx"; //исправить в будущем
-            SItemsListBox.PathFile[0] = @"C:\Users\Артём\Desktop\для тестов";
-            label1.Text = SItemsListBox.PathFile[0];
+            IPT.QuantityFiles = 1; //!! временно
+            if (IPT.QuantityFiles == 0 && i == 1 ) return; //i=1 первая кнопка 
+                                                           //разобраться почему не сработало String.Empty
+            IPT.PathFile[0] = @"C:\Users\Zhuravlev\Desktop\для проги\ИПТ4.xlsx"; //исправить в будущем
+            //SItemsListBoxIPT.PathFile[0] = @"C:\Users\Артём\Desktop\для тестов";
+            label1.Text = IPT.PathFile[0];
             
             switch (i)
             {
@@ -94,7 +127,7 @@ namespace excel
                     //Получаем набор ссылок на объекты Workbook
                     excelappworkbooks = excelapp.Workbooks;
                     //Открываем книгу и получаем на нее ссылку
-                    excelappworkbook = excelapp.Workbooks.Open(SItemsListBox.PathFile[0],
+                    excelappworkbook = excelapp.Workbooks.Open(IPT.PathFile[0],
                                        Type.Missing, Type.Missing, Type.Missing,
                      "WWWWW", "WWWWW", Type.Missing, Type.Missing, Type.Missing,
                       Type.Missing, Type.Missing, Type.Missing, Type.Missing,
@@ -105,13 +138,9 @@ namespace excel
                     excelsheets = excelappworkbook.Worksheets;
                     //Получаем ссылку на лист 1
                     excelworksheet = (Excel.Worksheet)excelsheets.get_Item(1);
-                             //Выбираем ряд для времени!!!!!!!!!!!
-                    
-                    excelcells = excelworksheet.get_Range("H11", "H11");
-                    label1.Text = Convert.ToString(excelcells.Value2);  //получаем число из экселя
-                    //Выводим число
-                    excelcells = excelworksheet.get_Range("E11", "H11");
-                    excelcells.Value2 = 10.5;
+                    //Выбираем ряд для времени!!!!!!!!!!!
+
+                        
                     //выбираю нужный формат
                     excelcells = excelworksheet.get_Range("C11", "C11");
                     //excelcells.NumberFormat = "ДД.ММ.ГГГГ ч:мм";
@@ -137,7 +166,7 @@ namespace excel
                     excelcells2 = excelworksheet.get_Range("B" + EndRange, "B" + EndRange);
 
                     //excelcells2[1, 1].EntireColumn.NumberFormat = "мм:сс";
-                    int CountHour = CountCells / 12 / 60; //12 т.к. в 1 мин 5сек по 12 раз //60 минут в часе
+                    //!!!!! проверить !! CountHour = CountCells / 12 / 60; //12 т.к. в 1 мин 5сек по 12 раз //60 минут в часе
 
                     //int CountSeconds = CountCells % 12;
                     // excelcells2[1, 1].EntireColumn.NumberFormat = "мм:сс";
@@ -152,38 +181,16 @@ namespace excel
                    excelcellsnew = excelworksheetnew.get_Range("B" + EndRange, "B" + EndRange); //куда будет записываться
 
                     //что нужно реализовать
-                    //1 подсчёт уол-ва файлов ИПТ и БУ отдельно
+                    //1 подсчёт кол-ва файлов ИПТ и БУ отдельно
                     //запись значений вынести как отдельную функцию
                     //с помощью свитча отправлять определённое кол-во раз в эту функцию
                     // менять куда будет записываться в виде аргументов в зависимости от свитча
-                    /*****************************Запись времени и значений***************************************************/
-                    for (int ih = 0; ih <= CountHour; ih++)
-                    {  
-                        for(int im = 0; im < (60); im++)
-                        {
-                            for(int isec = 0; isec < (12); isec++)
-                            {
-                                if ((CountCurentCell-1) == CountCells) break;
-                                excelcellsnew[CountCurentCell, 4].Value2 = excelcells2[CountCurentCellback, 4].Value2;// Т_3
-                                excelcellsnew[CountCurentCell, 3].Value2 = excelcells2[CountCurentCellback, 3].Value2; //D:D //Т_0
-                                excelcellsnew[CountCurentCell, 1].Value2 = excelcells2[CountCurentCellback, 2].Value2;//Bnew:C //время датчика
-                                excelcellsnew[CountCurentCell, 2].Value2 = Stime.hours[ih] + ":" + Stime.minutes[im] + ":" + Stime.seconds_05[isec];
-                                CountCurentCell++;
-                                CountCurentCellback--;
-                            }
-                        }
 
-                    }
-                    //excelcells2[1, 1].Value2 = Stime.seconds[0] + ":" + Stime.seconds[0] + ":" + Stime.seconds[1];
-                    //excelcells2[2, 1].Value2 = Stime.seconds[0] + ":" + Stime.seconds[0] + ":" + Stime.seconds[1];
-                    // excelcells2[1, 6].Value2 = Stime.second[1];
-
-                    /* потом вернуть
-                    //создание 
-                    excelapp.SheetsInNewWorkbook = 1;
-                    excelapp.Workbooks.Add(Type.Missing);
-                    */
+                    /*****************************Запись времени и значений для ИПТ***************************************************/
+                    if (IPT.PathFile[0] !=  String.Empty) writtenIPT(0); //вписать аргументы
+                    if (IPT.PathFile[1] != String.Empty) label102.Text = "chf,";
                     
+                 
                     break;
                 case 2:
                    // excelapp.Quit();
@@ -203,7 +210,7 @@ namespace excel
             }
                 
         }
-
+        /********************************* выбор файлов для ИПТ **********************************************************/
         private void listBox1_DragEnter(object sender, DragEventArgs e)
         {
             if (e.Data.GetDataPresent(DataFormats.FileDrop, false) == true)
@@ -223,14 +230,14 @@ namespace excel
                 //label1.Text = System.IO.Path.GetExtension(file);
                 if (System.IO.Path.GetExtension(file) == ".xls" || System.IO.Path.GetExtension(file) == ".xlsx" || System.IO.Path.GetExtension(file) == ".csv")
                 {
-                    SItemsListBox.PathFile[SItemsListBox.QuantityFiles] = file;
-                    SItemsListBox.Items[SItemsListBox.QuantityFiles] = System.IO.Path.GetFileName(file); //Только имя файла (с расширением):
+                    IPT.PathFile[IPT.QuantityFiles] = file;
+                    IPT.Items[IPT.QuantityFiles] = System.IO.Path.GetFileName(file); //Только имя файла (с расширением):
                                                                                                        // dbf_File = System.IO.Path.GetFileName(dbf_File);
-                    label1.Text = SItemsListBox.PathFile[SItemsListBox.QuantityFiles];
-                    listBox1.Items.Add(SItemsListBox.Items[SItemsListBox.QuantityFiles]);
+                    label1.Text = IPT.PathFile[IPT.QuantityFiles];
+                    listBox1.Items.Add(IPT.Items[IPT.QuantityFiles]);
                     //Только содержащий каталог:
                     //string dbf_Path = System.IO.Path.GetDirectoryName(dbf_File);
-                    SItemsListBox.QuantityFiles++;
+                    IPT.QuantityFiles++;
                 }
             }
         }
@@ -249,15 +256,15 @@ namespace excel
                 return;
             foreach (string file in dlg.FileNames)
             {
-                SItemsListBox.PathFile[SItemsListBox.QuantityFiles] = file;
-                SItemsListBox.Items[SItemsListBox.QuantityFiles] = System.IO.Path.GetFileName(file); //Только имя файла (с расширением):
+                IPT.PathFile[IPT.QuantityFiles] = file;
+                IPT.Items[IPT.QuantityFiles] = System.IO.Path.GetFileName(file); //Только имя файла (с расширением):
                                                                                                    // dbf_File = System.IO.Path.GetFileName(dbf_File);
-                label1.Text = SItemsListBox.PathFile[SItemsListBox.QuantityFiles];
-                listBox1.Items.Add(SItemsListBox.Items[SItemsListBox.QuantityFiles]);
-                SItemsListBox.QuantityFiles++;
+                label1.Text = IPT.PathFile[IPT.QuantityFiles];
+                listBox1.Items.Add(IPT.Items[IPT.QuantityFiles]);
+                IPT.QuantityFiles++;
             }
         }
-
+        /**************************/
         private void Form1_FormClosed(object sender, FormClosedEventArgs e)
         {
             try
@@ -268,10 +275,45 @@ namespace excel
             {
 
             }
+
+        }
+        /*****************************Запись времени и значений для ИПТ***************************************************/
+        void writtenIPT(int offset) // в аргументе смещение потому что будет записываться столбцы от каждого ИПТ и надо их свдвигать вправо
+        {
+            for (int ih = 0; ih <= 1; ih++)
+            {
+                if ((CountCurentCell - 1) == CountCells) break;
+                for (int im = 0; im < (60); im++)
+                {
+                    if ((CountCurentCell - 1) == CountCells) break;
+                    for (int isec = 0; isec < (12); isec++)
+                    {
+                        if ((CountCurentCell - 1) == CountCells) break;
+                        excelcellsnew[CountCurentCell, 6 + offset].Value2 = excelcells2[CountCurentCellback, 6].Value2;// Т_3
+                        excelcellsnew[CountCurentCell, 5 + offset].Value2 = excelcells2[CountCurentCellback, 5].Value2; //D:D //Т_0
+
+                        excelcellsnew[CountCurentCell, 1 + offset].EntireColumn.NumberFormat = "[$-ru-RU,1] ДД.ММ.ГГГГ ч:мм:сс";
+                         excelcellsnew[CountCurentCell, 1 + offset].Value2 = excelcells2[CountCurentCellback, 2].Value2;//Bnew:C //время датчика
+                        excelcellsnew[CountCurentCell, 2 + offset].Value2 = Stime.hours[ih] + ":" + Stime.minutes[im] + ":" + Stime.seconds_05[isec];
+                        CountCurentCell++;
+                        CountCurentCellback--;
+                    }
+                }
+
+            }
         }
 
-        
+
+
+
+
+
     }
+
+
+
+        
+    
 }
 
 
